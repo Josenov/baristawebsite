cb066772aea5ece3019fc2b46893fedb4782294a
Jose Maria Novillo <josenovillo88@gmail.com>
2024-04-02 11:22:32 -0300

    brand logo size fixed

--- src/components/BrandsCarousel.jsx
+++ src/components/BrandsCarousel.jsx
@@ -4,11 +4,11 @@ const BrandsCarousel = () => {
     return (
         <div className='flex items-center md:gap-5'>
             <img className='h-12 md:h-36' src="https://st4.depositphotos.com/25619466/27654/v/450/depositphotos_276541124-stock-illustration-creative-coffee-shop-concept-logo.jpg" alt="" />
-            <img className='h-12 md:h-24' src="https://cdn.kwork.com/files/portfolio/t3/15/040ab2cc7f4468125150f77e98e38b4b9e520522-1671119538.jpg" alt="" />
-            <img className='h-12 md:h-40' src="https://marketplace.canva.com/EADaj4foHd4/2/0/1600w/canva-white-and-black-shape-hipster-logo-mhyDVM6Th-Y.jpg" alt="" />
+            <img className='h-9 md:h-24' src="https://cdn.kwork.com/files/portfolio/t3/15/040ab2cc7f4468125150f77e98e38b4b9e520522-1671119538.jpg" alt="" />
+            <img className='h-14 md:h-40' src="https://marketplace.canva.com/EADaj4foHd4/2/0/1600w/canva-white-and-black-shape-hipster-logo-mhyDVM6Th-Y.jpg" alt="" />
             <img className='h-12 md:h-36' src="https://images.squarespace-cdn.com/content/v1/56a2e02b25981dd4cc3553dd/1485553875710-5OE8JBS32K7B81BD9SPJ/TheClassicLogo.jpg" alt="" />
             <img className='h-12 md:h-36' src="https://t4.ftcdn.net/jpg/05/14/65/27/360_F_514652787_7UJO5ONWkdDxPtBBmyj3oslXE96kilon.jpg" alt="" />
-            <img className='h-12 md:h-28' src="https://img.freepik.com/vector-premium/rey-letras-tipografia-letras-vector_635759-354.jpg" alt="" />
+            <img className='h-10 md:h-28' src="https://img.freepik.com/vector-premium/rey-letras-tipografia-letras-vector_635759-354.jpg" alt="" />
         </div>
     )
 }
