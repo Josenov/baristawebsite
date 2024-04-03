d3a8902e0146f4fbd410b58eecc0fe1eef979422
Jose Maria Novillo <josenovillo88@gmail.com>
2024-04-03 11:34:08 -0300

    font style carousel added

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Carousel = () => {
   return (
-    <div className=''>
+    <div className='font-pro'>
         <img className='object-cover h-full w-full  md:w-full md:h-[480px]' src="https://www.somoselcafe.com.ar/img/novedades/14.webp" alt="" />
-        <div className='mt-[-150px] md:mt-[-300px] md:ml-[200px] text-white font-semibold pl-5'>
+        <div className='mt-[-150px] md:mt-[-300px] md:ml-[200px] text-white font-thun pl-5'>
         <p className='text-[#C8A178]'>Cafe delicioso</p>
         <p className='text-2xl md:text-5xl md:mt-2'>Cafe Fresco Tostado</p>
-        <button className='absolute md:mt-5 bg-[#C8A178] text-white  p-2 rounded-full w-36 h-8 mt-3 font-semibold flex items-center justify-center'>Comprar Ahora</button>
+        <button className='absolute md:mt-5 bg-[#C8A178] text-white  p-2 rounded-full w-36 h-8 mt-3 font-thin flex items-center justify-center'>Comprar Ahora</button>
         </div>
     </div>
   )
