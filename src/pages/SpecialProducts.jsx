51c2b6bc56b9d8f2f7d4b17e3234fa0011b9451c
Jose Maria Novillo <josenovillo88@gmail.com>
2024-04-25 21:19:32 -0300

    special products from database connected

--- src/pages/SpecialProducts.jsx
+++ src/pages/SpecialProducts.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+
 import SpecialProductCard from '../components/SpecialProductCard'
 
 const SpecialProducts = () => {
+   
   return (
     <div className='h-screen flex items-center justify-center'>
-        <SpecialProductCard/>
+      
+            <SpecialProductCard />
+          
+        
     </div>
   )
 }
